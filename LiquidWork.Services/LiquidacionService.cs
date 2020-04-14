0dbc0a5030fc2a9e57716347e6da3ff0770039e6
tomasbrunog <tomasbrunog@email.com>
2020-04-13 22:32:13 -0300

    Clarify LiquidacionService methods

--- LiquidWork.Services/LiquidacionService.cs
+++ LiquidWork.Services/LiquidacionService.cs
@@ -24,38 +24,44 @@ namespace LiquidWork.Services
 
             UpdateNeto(liquidacion);
         }
+
         public async void UpdateNeto(Concepto newConcepto)
         {
             var liquidacion = await _context.Liquidaciones
                 .Include(li => li.Conceptos)
                 .FirstOrDefaultAsync(li => li.LiquidacionId == newConcepto.LiquidacionId);
 
-            liquidacion.Neto = 0;
+            decimal? neto = 0;
             foreach (var item in liquidacion.Conceptos)
             {
-                liquidacion.Neto += item.Monto;
+                neto += item.Monto;
             }
+            liquidacion.Neto = neto;
         }
+
         public async void UpdateNeto(int? liquidacionId)
         {
             var liquidacion = await _context.Liquidaciones
                 .Include(li => li.Conceptos)
                 .FirstOrDefaultAsync(li => li.LiquidacionId == liquidacionId);
 
-            liquidacion.Neto = 0;
+            decimal? neto = 0;
             foreach (var item in liquidacion.Conceptos)
             {
-                liquidacion.Neto += item.Monto;
+                neto += item.Monto;
             }
+            liquidacion.Neto = neto;
         }
 
         public void UpdateNeto(Liquidacion liquidacion)
         {
-            liquidacion.Neto = 0;
+            decimal? neto = 0;
             foreach (var item in liquidacion.Conceptos)
             {
-                liquidacion.Neto += item.Monto;
+                neto += item.Monto;
             }
+            liquidacion.Neto = neto;
+
         }
 
 
